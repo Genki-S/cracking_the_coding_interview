2fc56f652f6feb1564399533599c1591ed870410
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-12-11 18:45:28 +0900

    Add a spec for initialization with data

--- p57/heap_spec.rb
+++ p57/heap_spec.rb
@@ -4,25 +4,35 @@ require_relative './heap'
 describe Heap do
   let(:data) { [*-100..100].sample(10) }
 
-  before do
-    data.each do |datum|
-      heap.push(datum)
-    end
-  end
+  context 'when it is initialized with data' do
+    subject(:heap) { Heap.new(data, &:<) }
 
-  context 'when it is initialized with &:<' do
-    subject(:heap) { Heap.new(&:<) }
-
-    it 'creates a minimum heap' do
+    it 'creates a heap' do
       expect(heap.to_a).to eq(data.sort)
     end
   end
 
-  context 'when it is initialized with &:>' do
-    subject(:heap) { Heap.new(&:>) }
+  context 'whne data is pushed one by one' do
+    before do
+      data.each do |datum|
+        heap.push(datum)
+      end
+    end
 
-    it 'creates a maximum heap' do
-      expect(heap.to_a).to eq(data.sort{ |a, b| b <=> a })
+    context 'when it is initialized with &:<' do
+      subject(:heap) { Heap.new(&:<) }
+
+      it 'creates a minimum heap' do
+        expect(heap.to_a).to eq(data.sort)
+      end
+    end
+
+    context 'when it is initialized with &:>' do
+      subject(:heap) { Heap.new(&:>) }
+
+      it 'creates a maximum heap' do
+        expect(heap.to_a).to eq(data.sort{ |a, b| b <=> a })
+      end
     end
   end
 end
